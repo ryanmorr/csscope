8486d8c29f4854de0d2d7688c9fd59bd6f6e8c06
Ryan Morr <rm.morr@gmail.com>
2019-06-19 14:41:22 -0400

    improve tests

--- test/specs/css.js
+++ test/specs/css.js
@@ -48,7 +48,7 @@ describe('css', () => {
                 background-image:url("foo.jpg"), url("bar.jpg"), url("baz.jpg");
                 font-family:Times, "Times New Roman", serif;
                 width:calc(30% * 20em - 2vh / 2pt);
-                box-shadow:inset 0 0 10px rgba(0,0,0,.5);
+                box-shadow:inset 0 0 10px rgba(0,0,0,.5) !important;
                 transform:rotate(-45deg) skew(20deg, 40deg) scale(2);
             }
         `, `
@@ -58,7 +58,7 @@ describe('css', () => {
                 background-image:url("foo.jpg"), url("bar.jpg"), url("baz.jpg");
                 font-family:Times, "Times New Roman", serif;
                 width:calc(30% * 20em - 2vh / 2pt);
-                box-shadow:inset 0 0 10px rgba(0,0,0,.5);
+                box-shadow:inset 0 0 10px rgba(0,0,0,.5) !important;
                 transform:rotate(-45deg) skew(20deg, 40deg) scale(2);
             }
         `);
@@ -66,46 +66,67 @@ describe('css', () => {
 
     it('should transform a stylesheet', () => {
         expectCSS(`
-            .foo {
+            /**
+             * Proin ut iaculis eros, sed viverra leo. Maecenas et consequat dui. Nunc. 
+             */
+            #foo {
                 width: 100px;
             }
 
-            .bar {
+            /**
+             * Pellentesque feugiat est nec laoreet volutpat. Mauris augue justo, tincidunt ut tempor. 
+             */
+            .bar.baz {
                 width: 200px;
                 background-color: red;
             }
 
+            /**
+             * Cras pulvinar tempor dolor in pretium. Donec at sapien sem. Proin id. 
+             */
             ol ol,
             ul ul {
                 margin: 0.4em 0;
             }
 
+            /**
+             * Morbi maximus nibh massa, quis tempor eros tempor nec. Nunc est risus. 
+             */
             #foo > [attr=val]:empty + div.foo.bar,
             :not(span[attr]:contains("foo")) {
                 display: block;
             }
 
             @media screen and (max-width: 480px) {
-                .foo {
+                /**
+                 * Nullam ut mauris quam. Aliquam leo mi, ultricies at justo non, pharetra. 
+                 */
+                #foo {
                     width: 200px;
                     height: 300px;
                 }
 
+                /**
+                 * Praesent ac arcu a libero faucibus auctor eu vitae elit. Nam vulputate. 
+                 */
                 div#foo.bar[attr=val]:empty,
                 :not(#foo.bar[attr]:empty) {
                     display: flex;
                 }
 
+                /**
+                 * Proin quis ipsum efficitur, aliquam dui vel, ultrices enim. Aliquam eget ligula. 
+                 */
                 ol ol,
                 ul + ul {
                     margin: 1.4em 1em;
                 }
             }
         `, `
-            .foo[data-css-foo]{
+            #foo[data-css-foo]{
                 width:100px;
             }
-            .bar[data-css-foo]{
+            .bar.baz[data-css-foo]{
                 width:200px;
                 background-color:red;
             }
@@ -116,7 +137,7 @@ describe('css', () => {
                 display:block;
             }
             @media screen and (max-width:480px){
-                .foo[data-css-foo]{
+                #foo[data-css-foo]{
                     width:200px;
                     height:300px;
                 }
